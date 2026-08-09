91f4c33335ada1d89934271937f560e6eae5e592
vivek96181-dev <vivek96181@gmail.com>
2026-08-09 22:10:04 +0530

    Time: 4 ms (19.35%) | Memory: 46 MB (98.41%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        // TreeNode r=is(root,subRoot.val);
+        // return isTheir(r,subRoot);
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        boolean is=false;
+        while(!q.isEmpty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                TreeNode ne=q.poll();
+                if(ne.val==subRoot.val){
+                    if(isTheir(ne,subRoot)) return true;
+                    is=isTheir(ne,subRoot);
+                }
+                if(ne.left!=null) q.add(ne.left);
+                if(ne.right!=null) q.add(ne.right);
+
+            }
+        }
+        return is;
+    }
+    // public TreeNode is(TreeNode root,int value){
+    //     if(root==null){
+    //         return new TreeNode();
+    //     }
+    //     if(root.val==value) return root;
+    //     is(root.right,value);
+    //     is(root.left,value);
+    //     return new TreeNode();
+        
+    // }
+    public boolean isTheir(TreeNode root,TreeNode subRoot){
+        if(root==null && subRoot==null) return true;
+        else if((root==null && subRoot!=null) || (root!=null && subRoot==null)) return false;
+        if(root.val!=subRoot.val) return false;
+        boolean l=isTheir(root.left,subRoot.left);
+        boolean r=isTheir(root.right,subRoot.right);
+        return l&&r;
+    }
+}
