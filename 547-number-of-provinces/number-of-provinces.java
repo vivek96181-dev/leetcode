7d1ea236dca0ca0352ccc41bd5b10e2c74e3b411
vivek96181-dev <vivek96181@gmail.com>
2026-08-10 11:16:51 +0530

    Time: 1 ms (86.48%) | Memory: 47.4 MB (44.03%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,25 +1,37 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int m=isConnected.length;
-        int n=isConnected[0].length;
+        int n=isConnected.length;
+        boolean[] visited=new boolean[n];
         int c=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1){
-                    dfs(isConnected,i,n);
-                    c++;
-                }
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(isConnected[i][j]==1){
+        //             dfs(isConnected,i,n);
+        //             c++;
+        //         }
+        //     }
+        // }
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(isConnected,i,visited);
+                c++;
             }
         }
         return c;
 
     }
-    public void dfs(int[][] isConnected,int i,int n){
-        isConnected[i][i]=0;
-        for(int j=0;j<n;j++){
-            if(isConnected[i][j]==1){
-                isConnected[i][j]=0;
-                dfs(isConnected,j,n);
+    public void dfs(int[][] isConnected,int i,boolean[] visited){
+        // isConnected[i][i]=0;
+        // for(int j=0;j<n;j++){
+        //     if(isConnected[i][j]==1){
+        //         isConnected[i][j]=0;
+        //         dfs(isConnected,j,n);
+        //     }
+        // }
+        visited[i]=true;
+        for(int j=0;j<isConnected.length;j++){
+            if(isConnected[i][j]==1 && !visited[j]){
+                dfs(isConnected,j,visited);
             }
         }
     }
