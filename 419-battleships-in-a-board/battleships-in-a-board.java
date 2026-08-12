1ae3bb052c224ade2815c9a9c12f485a7a2460ae
vivek96181-dev <vivek96181@gmail.com>
2026-08-12 23:55:22 +0530

    Time: 1 ms (98.81%) | Memory: 45.6 MB (53.06%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.java
+++ 419-battleships-in-a-board/battleships-in-a-board.java
@@ -4,53 +4,63 @@ class Solution {
         int n=board[0].length;
         int ans=0;
         for(int i=0;i<m;i++){
-            
-            
             for(int j=0;j<n;j++){
                 if(board[i][j]=='X'){
-                    int c=0;
-                    boolean k=false;
-                    if(i>0 && board[i-1][j]=='X'){
-                        c++;
-                        k=true;
-                    }
-                    if(j>0 && board[i][j-1]=='X'){
-                        c++;
-                        
-                    }
-                    if(i<m-1 && board[i+1][j]=='X'){
-                        c++;
-                        k=true;
-                    }
-                    if(j<n-1 && board[i][j+1]=='X'){
-                        c++;
-                        
-                    }
-                    if(c>1) continue;
-                    if(k && dfsr(board,i,j,m,n)){
-                        ans++;
-                    }else if(!k && dfsc(board,i,j,m,n)){
-                        ans++;
-                    }
+                    if(i>0 && board[i-1][j]=='X') continue;
+                    if(j>0 && board[i][j-1]=='X') continue;
+                    ans++;
                 }
             }
         }
         return ans;
+    }
+}    
 
-    }
-    public boolean dfsr(char[][] board,int i,int j,int m,int n){
-        if(i<0 || j<0 || i>=m || j>=n || board[i][j]!='X') return true;
+    //     for(int i=0;i<m;i++){
+    //         for(int j=0;j<n;j++){
+    //             if(board[i][j]=='X'){
+    //                 int c=0;
+    //                 boolean k=false;
+    //                 if(i>0 && board[i-1][j]=='X'){
+    //                     c++;
+    //                     k=true;
+    //                 }
+    //                 if(j>0 && board[i][j-1]=='X'){
+    //                     c++;
+                        
+    //                 }
+    //                 if(i<m-1 && board[i+1][j]=='X'){
+    //                     c++;
+    //                     k=true;
+    //                 }
+    //                 if(j<n-1 && board[i][j+1]=='X'){
+    //                     c++;
+                        
+    //                 }
+    //                 if(c>1) continue;
+    //                 if(k && dfsr(board,i,j,m,n)){
+    //                     ans++;
+    //                 }else if(!k && dfsc(board,i,j,m,n)){
+    //                     ans++;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return ans;
+
+    // }
+    // public boolean dfsr(char[][] board,int i,int j,int m,int n){
+    //     if(i<0 || j<0 || i>=m || j>=n || board[i][j]!='X') return true;
         
-        // if(!k && (board[i-1][j]=='X' || board[i+1][j]=='X')) return false;
-        if(j<n-1 && board[i][j+1]=='X' || j>0 && board[i][j-1]=='X') return false;
-        board[i][j]='.';
-        return dfsr(board,i+1,j,m,n);
-    }
-    public boolean dfsc(char[][] board,int i,int j,int m,int n){
-        if(i<0 || j<0 || i>=m || j>=n || board[i][j]!='X') return true;
-        // if(k && (board[i][j+1]=='X' || board[i][j-1]=='X')) return false;
-        if(i>0 && board[i-1][j]=='X' || i<m-1 && board[i+1][j]=='X') return false;
-        board[i][j]='.';
-        return dfsc(board,i,j+1,m,n);
-    }
-}
+    //     // if(!k && (board[i-1][j]=='X' || board[i+1][j]=='X')) return false;
+    //     if(j<n-1 && board[i][j+1]=='X' || j>0 && board[i][j-1]=='X') return false;
+    //     board[i][j]='.';
+    //     return dfsr(board,i+1,j,m,n);
+    // }
+    // public boolean dfsc(char[][] board,int i,int j,int m,int n){
+    //     if(i<0 || j<0 || i>=m || j>=n || board[i][j]!='X') return true;
+    //     // if(k && (board[i][j+1]=='X' || board[i][j-1]=='X')) return false;
+    //     if(i>0 && board[i-1][j]=='X' || i<m-1 && board[i+1][j]=='X') return false;
+    //     board[i][j]='.';
+    //     return dfsc(board,i,j+1,m,n);
+     
