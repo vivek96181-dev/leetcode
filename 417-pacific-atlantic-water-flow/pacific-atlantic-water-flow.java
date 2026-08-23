9c297bc651856964e03a8e57202a08be2490d4e4
vivek96181-dev <vivek96181@gmail.com>
2026-08-23 23:34:10 +0530

    Time: 5 ms (53.37%) | Memory: 47.4 MB (72.86%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -6,26 +6,25 @@ class Solution {
         int n=heights[0].length;
         boolean[][] pacific=new boolean[m][n];
         for(int i=0;i<n;i++){
-                
-                dfs(heights,0,i,-1,-1,m,n,pacific);
+                if(!pacific[0][i])
+                    dfs(heights,0,i,-1,-1,m,n,pacific);
             
         }
         for(int i=0;i<m;i++){
-                
-                dfs(heights,i,0,-1,-1,m,n,pacific);
+                if(!pacific[i][0])
+                    dfs(heights,i,0,-1,-1,m,n,pacific);
             
         }
         boolean[][] atlantic=new boolean[m][n];
         for(int i=0;i<n;i++){
            
-                
-                dfs(heights,m-1,i,-1,-1,m,n,atlantic);
+                if(!atlantic[m-1][i])
+                    dfs(heights,m-1,i,-1,-1,m,n,atlantic);
             
         }
         for(int i=0;i<m;i++){
-            
-                
-                dfs(heights,i,n-1,-1,-1,m,n,atlantic);
+                if(!atlantic[i][n-1])
+                    dfs(heights,i,n-1,-1,-1,m,n,atlantic);
             
         }
         for(int i=0;i<m;i++){
@@ -49,56 +48,5 @@ class Solution {
 
     }
 
-    // public boolean dfs(int[][] heights,int i,int j,int p,int p1,int m,int n,boolean[][] visited){
-    //     if(i==-1 || j==-1) return true;
-    //     if(i==m || j==n){
-    //         is=true;
-    //         return false;
-    //     }
-    //     if(visited[i][j]) return false;
-    //     if(p!=-2 && p1!=-2 && heights[i][j]>heights[p][p1]) return false;
-    //     visited[i][j]=true;
-        
-        
-    //        boolean r1=dfs(heights,i-1,j,i,j,m,n,visited);
-                 
-        
-    //        boolean r2=dfs(heights,i+1,j,i,j,m,n,visited);
-         
-     
-    //        boolean c1=dfs(heights,i,j-1,i,j,m,n,visited);
-         
-        
-    //        boolean c2=dfs(heights,i,j+1,i,j,m,n,visited);
-        
-    //     visited[i][j]=false;
-        
-    //     return  r1||r2||c1||c2;
-    // }
-    // public boolean dfs1(int[][] heights,int i,int j,int p,int p1,int m,int n,boolean is){
-    //     if(i==m || j==n){
-            
-    //         return true;
-    //     }
-    //     if(i==-1 || j==-1) return false;
-        
-    //     if(p!=-1 && p!=-1 && heights[i][j]>heights[p][p1]) return false;
-    //     boolean r1=false;
-    //     boolean r2=false; 
-    //     boolean c1=false; 
-    //     boolean c2=false;
-    //     if(i-1!=p && j!=p1){
-    //         r1=dfs(heights,i-1,j,i,j,m,n,is);
-    //     } 
-    //     if(i+1!=p && j!=p1){
-    //         r2=dfs(heights,i+1,j,i,j,m,n,is);
-    //     } 
-    //     if(i!=p && j-1!=p1){
-    //         c1=dfs(heights,i,j-1,i,j,m,n,is);
-    //     } 
-    //     if(i!=p && j+1!=p1){
-    //         c2=dfs(heights,i,j+1,i,j,m,n,is);
-    //     } 
-    //     return  r1||r2||c1||c2;
-    // }
+    
 }
